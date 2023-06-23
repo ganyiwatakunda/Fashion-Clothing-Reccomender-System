d20437cca6fefe86132ee09d3c5fcfab614cfd5a
ganyiwatakunda <118890097+ganyiwatakunda@users.noreply.github.com>
2023-06-23 23:56:51 +0200

    Update main1.py

--- main1.py
+++ main1.py
@@ -53,7 +53,7 @@ def feature_extraction(img_path,model):
 def recommend_brt(features, seq_length, model, feature_list, filenames):
     # Create a sequence of feature vectors
     seq = []
-    seq_length = length(seq)
+    seq_length = len(seq)
     for i in range(seq_length):
         seq.append(features)
     seq = np.array(seq)
